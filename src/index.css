943e3ef6d05fe9c137c8652e560263edb8a0e65f
CtrlFrontfire <vlachaki.argyro@gmail.com>
2024-07-01 11:12:21 +0100

    Pretty weather app CSS loaded

---
+++ src/index.css
@@ -0,0 +1,203 @@
+body {
+  background-color: #6643b5;
+  font-family: "Roboto", sans-serif;
+}
+a {
+  color: #6643b5;
+}
+h1 {
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 100%);
+  color: transparent;
+  margin: 5px 0 10px 0;
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+header {
+  padding: 10px 0 10px 0;
+}
+main {
+  padding-top: 25px;
+  padding-bottom: 25px;
+}
+.weather-app {
+  background: #8594e4;
+  margin: 30px auto;
+  padding: 40px;
+  max-width: 600px;
+  border: 1px solid #8594e4;
+  border-radius: 10px;
+  box-shadow: 0 30px 50px rgba(65, 50, 100, 0.774);
+}
+
+form {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.search-form-input {
+  background-color: #d5def59c;
+  border: 1px solid #430f585b;
+  border-radius: 5px;
+  font-size: 15px;
+  width: 80%;
+  padding: 15px 20px;
+}
+input::placeholder {
+  color: #430f5881;
+}
+
+.search-button {
+  background: #6643b5;
+  color: #d5def5;
+  border: none;
+  border-radius: 5px;
+  padding: 15px 27px;
+  font-size: 16px;
+  transition: all 250ms ease-in-out;
+}
+.search-button:hover {
+  background: #d5def5;
+  color: #6643b5;
+  cursor: pointer;
+  box-shadow: 0px 0px 7px 1px rgba(91, 63, 159, 0.774);
+  font-weight: bold;
+}
+
+.weather-app-data {
+  display: flex;
+  flex-direction: column;
+}
+.temperature-container {
+  display: flex;
+  padding: 10px;
+  flex-direction: column;
+  align-items: center;
+}
+.weather-app-temperature {
+  display: flex;
+}
+.weather-app-condition {
+  font-size: 18px;
+  letter-spacing: 0.2rem;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 85%);
+  color: transparent;
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.temperature-unit {
+  display: flex;
+  margin: 5px 0 0 0;
+  height: 80px;
+}
+.weather-app-temperature {
+  font-size: 65px;
+  font-weight: bold;
+  margin-top: 5px;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 65%);
+  color: transparent;
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.weather-app-unit {
+  padding-top: 5px;
+  margin-top: 5px;
+  font-size: 45px;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 100%);
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.condition-icon {
+  width: 190px;
+  height: 190px;
+  display: flex;
+  justify-content: center;
+  filter: drop-shadow(0px 0px 20px rgb(255, 255, 255));
+}
+
+.weather-app-city {
+  font-size: 30px;
+  padding: 0 0 5px 0;
+  font-weight: bold;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 65%);
+  color: transparent;
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.weather-app-timestamp {
+  font-size: 18px;
+  letter-spacing: 0.1rem;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 85%);
+  color: transparent;
+  text-align: center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.weather-app-details {
+  display: block;
+  margin: 20px auto 10px;
+  column-count: 2;
+  column-fill: balance;
+  font-size: 20px;
+}
+.real-feel,
+.pressure,
+.wind-speed,
+.humidity-levels {
+  text-align: center;
+  width: 150px;
+  padding: 5px 10px 10px 10px;
+  background-image: linear-gradient(0deg, transparent 0, #f2f2f2 100%);
+  color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.weather-forecast {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding-top: 25px;
+  border-top: 1px solid #f9f7fe33;
+}
+
+.weather-forecast-date {
+  color: #f2f2f2;
+  opacity: 80%;
+  text-align: center;
+  margin-bottom: 5px;
+}
+
+.weather-forecast-icon {
+  display: block;
+  margin: 0 auto;
+  filter: drop-shadow(0px 0px 7px rgb(255, 255, 255));
+}
+
+.weather-forecast-temperatures {
+  color: #f2f2f2;
+  opacity: 80%;
+  text-align: center;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 10px auto;
+}
+.weather-forecast-temperature {
+  padding: 0 8px;
+}
+
+footer {
+  border-top: 1px solid #f9f7fe33;
+  padding: 30px 0 0 0;
+  font-size: 14px;
+  text-align: center;
+  color: white;
+  opacity: 65%;
+}
